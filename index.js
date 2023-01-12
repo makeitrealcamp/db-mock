5d4bd8521736aab795522684df2030ecb1dbf33a
khriztianmoreno <khriztianmoreno@gmail.com>
2023-01-12 16:42:39 -0500

    test: :white_check_mark: add tests

--- index.js
+++ index.js
@@ -1,30 +1,46 @@
 const records = {}
-const nextId = 1
+let nextId = 1
 
-export function findAll() {
+function findAll() {
   return Object.values(records).map(record => ({ ...record }))
 }
 
-export function findById(id) {
+function findById(id) {
   return { ...records[id] }
 }
 
-export function insert(record) {
+function insert(record) {
   record.id = nextId
   records[nextId] = { ...record }
   nextId++
   return record
 }
 
-export function update(record) {
+function update(record) {
   const id = record.id
   if (!id) throw new Error("The record doesn't have an id property")
   if (!records[id]) {
     throw new Error(`The record with id ${id} was not found`)
   }
   records[id] = record
+
+  return record
 }
 
-export function remove(id) {
+function remove(id) {
   delete records[id]
+}
+
+function reset() {
+  Object.keys(records).forEach(key => delete records[key])
+  nextId = 1
+}
+
+module.exports = {
+  findAll,
+  findById,
+  insert,
+  update,
+  remove,
+  reset,
 }
